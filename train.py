c3ba9ff7e2aaef497a95c9b3330a96763456f099
Kan Tippayamontri <kantip@MacBook-Pro-khxngKan-5.local>
2023-09-19 13:58:24 +0700

    prepare to add comet_ml

--- train.py
+++ train.py
@@ -9,18 +9,18 @@ dataset = YOLODataset(
     ],
     remove_exists=True,
 )
-# dataset.change_settings_dataset()
 
-# print(f"dataset train : {dataset.dataset_train_path}")
 dataset.show_samples(
     number_of_samples=15, datasetTrainValTest=Constants.DatasetTrainValTest.VAL
 )
+
 yolo_model = YOLOModel(
     model_type=Constants.ModelType.NANO,
     use_comet=True,
     dataset_type=Constants.DatasetType.TYPE_3,
     dataset_use=Constants.DatasetUse.TYPE_3_GAUGE_DISPLAY_FRAME,
 )
+
 print(f"data yaml path: {dataset.get_data_yaml4train()}")
 yolo_model.train(
     parameters=TrainParameters(
