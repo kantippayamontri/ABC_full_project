cd12b647469f352fa54e97794e7db008e28927fd
Kan <kantippayamontri@gmail.com>
2024-03-12 17:06:14 +0700

    add more in transform.py

--- preprocess/transforms.py
+++ preprocess/transforms.py
@@ -59,7 +59,7 @@ class Transform:
         self,
         img,
         bb,
-        class_crop,
+        class_crop_list,
         need_resize,
         target_size,
         class_ignore,
@@ -76,95 +76,95 @@ class Transform:
             bb = []  # for store bb use after crop
             bb_crop = []  # for store bb that use to crop the coordinate
 
-        for _bb in bb_temp:
-            if _bb[-1] == class_crop:
-                bb_crop.append(_bb)
-                bb.append(_bb)  # also add class crop into bb
-            elif _bb[-1] not in class_ignore:
-                bb.append(_bb)
+            for _bb in bb_temp:
+                if int(_bb[-1]) == int(class_crop):
+                    bb_crop.append(_bb)
+                    bb.append(_bb)  # also add class crop into bb
+                elif _bb[-1] not in class_ignore:
+                    bb.append(_bb)
 
-        bb = np.array(bb)
-        bb_crop = np.array(bb_crop)
+            bb = np.array(bb)
+            bb_crop = np.array(bb_crop)
 
-        if len(bb_crop) == 0:
-            return img, bb
+            if len(bb_crop) == 0:
+                return img, bb
 
-        for index_crop, _bbc in enumerate(
-            bb_crop
-        ):  # loop image to get new image that crop from _bbc
-            bb_use = []
-            for _bb in bb:  # loop check which bb inside the _bbc
-                _, bb1 = self.prepare_output(
+            for index_crop, _bbc in enumerate(
+                bb_crop
+            ):  # loop image to get new image that crop from _bbc
+                bb_use = []
+                for _bb in bb:  # loop check which bb inside the _bbc
+                    _, bb1 = self.prepare_output(
+                        img=None, bb=np.array([_bbc])
+                    )  # for bb crop
+                    box1 = Utils.change_format_yolo2xyxy(
+                        img_size=full_img.shape, bb=bb1[0], with_class=True
+                    )["bb"]
+
+                    __, bb2 = self.prepare_output(
+                        img=None, bb=np.array([_bb])
+                    )  # for bb use
+                    box2 = Utils.change_format_yolo2xyxy(
+                        img_size=full_img.shape, bb=bb2[0], with_class=True
+                    )["bb"]
+
+                    if Utils.calculate_intersection(box1=box1, box2=box2):
+                        bb_use.append(_bb.copy())
+
+                if len(bb_use) == 0:
+                    continue
+
+                bb_use = np.array(bb_use)
+
+                _, _bbc = self.prepare_output(
                     img=None, bb=np.array([_bbc])
-                )  # for bb crop
-                box1 = Utils.change_format_yolo2xyxy(
-                    img_size=full_img.shape, bb=bb1[0], with_class=True
-                )["bb"]
+                )  # _bbc need this format [c,x,y,w,h] -> use change format from yolo to xyxy
+                _bbc = _bbc[0]
 
-                __, bb2 = self.prepare_output(
-                    img=None, bb=np.array([_bb])
-                )  # for bb use
-                box2 = Utils.change_format_yolo2xyxy(
-                    img_size=full_img.shape, bb=bb2[0], with_class=True
-                )["bb"]
-
-                if Utils.calculate_intersection(box1=box1, box2=box2):
-                    bb_use.append(_bb.copy())
-
-            if len(bb_use) == 0:
-                continue
-
-            bb_use = np.array(bb_use)
-
-            _, _bbc = self.prepare_output(
-                img=None, bb=np.array([_bbc])
-            )  # _bbc need this format [c,x,y,w,h] -> use change format from yolo to xyxy
-            _bbc = _bbc[0]
-
-            albu_transform = Utils.albu_crop_img_bb(
-                img=full_img,
-                bb_crop=_bbc,
-                format=Constants.BoundingBoxFormat.YOLOV8,
-                add_pixels=random.randint(0, pixel_added),
-                with_class=True,
-            )
-
-            transformed = albu_transform(image=full_img, bboxes=bb_use)
-            new_img, new_bb = self.get_output_tramsformed(transformed=transformed)
-
-            if need_resize:
-                new_img, new_bb = self.resize(
-                    img=new_img,
-                    bb=new_bb,
-                    target_size=target_size,
+                albu_transform = Utils.albu_crop_img_bb(
+                    img=full_img,
+                    bb_crop=_bbc,
                     format=Constants.BoundingBoxFormat.YOLOV8,
+                    add_pixels=random.randint(0, pixel_added),
+                    with_class=True,
                 )
-                new_bb = np.array([list(bb_temp) for bb_temp in new_bb])
 
-            # new name for crop
-            new_name_img = self.make_new_name(
-                name=self.img_path.name,
-                function_name="crop",
-                prefix=f"{class_crop}_{index_crop}",
-            )
-            new_name_bb = self.make_new_name(
-                name=self.bb_path.name,
-                function_name="crop",
-                prefix=f"{class_crop}_{index_crop}",
-            )
+                transformed = albu_transform(image=full_img, bboxes=bb_use)
+                new_img, new_bb = self.get_output_tramsformed(transformed=transformed)
 
-            # save crop image
-            self.save_img(
-                img=new_img, path=save_path / Constants.image_folder / new_name_img
-            )
-            # save crop bb
-            _, new_bb_save = self.prepare_output(img=None, bb=new_bb)
-            self.save_bb(
-                bb_list=new_bb_save,
-                path=save_path / Constants.label_folder / new_name_bb,
-            )
+                if need_resize:
+                    new_img, new_bb = self.resize(
+                        img=new_img,
+                        bb=new_bb,
+                        target_size=target_size,
+                        format=Constants.BoundingBoxFormat.YOLOV8,
+                    )
+                    new_bb = np.array([list(bb_temp) for bb_temp in new_bb])
 
-            return img, bb # for crop return original image and bb
+                # new name for crop
+                new_name_img = self.make_new_name(
+                    name=self.img_path.name,
+                    function_name="crop",
+                    prefix=f"{class_crop}_{index_crop}",
+                )
+                new_name_bb = self.make_new_name(
+                    name=self.bb_path.name,
+                    function_name="crop",
+                    prefix=f"{class_crop}_{index_crop}",
+                )
+
+                # save crop image
+                self.save_img(
+                    img=new_img, path=save_path / Constants.image_folder / new_name_img
+                )
+                # save crop bb
+                _, new_bb_save = self.prepare_output(img=None, bb=new_bb)
+                self.save_bb(
+                    bb_list=new_bb_save,
+                    path=save_path / Constants.label_folder / new_name_bb,
+                )
+
+        return img, bb
 
     def resize(self, img, bb, target_size, format=None):
         albu_transform = Utils.albu_resize_img_bb(
@@ -233,6 +233,20 @@ class Transform:
         new_img, new_bb = self.get_output_tramsformed(transformed=transformed)
         return new_img, new_bb
 
+    def longest_max_size(self, img, bb, format=None, p=1.0, max_size=640):
+        albu_transform = Utils.albu_longest_max_size(
+            format=format, p=p, max_size=max_size
+        )
+        transformed = albu_transform(image=img, bboxes=bb)
+        new_img, new_bb = self.get_output_tramsformed(transformed=transformed)
+        return new_img, new_bb
+
+    def pad_if_needed(self, img, bb, format=None, p=1.0, min_width=640, min_height=640):
+        albu_transform = Utils.albu_pad_if_needed(format=format, p=p, min_width=min_width, min_height=min_height)
+        transformed = albu_transform(image=img, bboxes=bb)
+        new_img, new_bb = self.get_output_tramsformed(transformed=transformed)
+        return new_img, new_bb
+
     def transform_dict_function(
         self, function_name, function_parameter, img, bb, target_folder_path
     ):
@@ -243,7 +257,7 @@ class Transform:
             img, bb = self.crop(
                 img=img.copy(),
                 bb=bb.copy(),
-                class_crop=function_parameter["CLASS_CROP_LIST"],
+                class_crop_list=function_parameter["CLASS_CROP_LIST"],
                 need_resize=function_parameter["NEED_RESIZE"],
                 target_size=(
                     function_parameter["TARGET_WIDTH"],
@@ -309,13 +323,32 @@ class Transform:
                 format=Constants.BoundingBoxFormat.YOLOV8,
                 p=function_parameter["P"],
                 brightness=function_parameter["BRIGHTNESS"],
-                contrast=function_parameter["CONTRAST"], 
+                contrast=function_parameter["CONTRAST"],
                 saturation=function_parameter["SATURATION"],
                 hue=function_parameter["HUE"],
             )
+        elif function_name == "LONGEST_MAX_SIZE":
+            img, bb = self.longest_max_size(
+                img=img.copy(),
+                bb=bb.copy(),
+                format=Constants.BoundingBoxFormat.YOLOV8,
+                p=function_parameter["P"],
+                max_size=function_parameter["MAX_SIZE"],
+            )
+        elif function_name == "PAD_IF_NEEDED":
+            img, bb = self.pad_if_needed(
+                img=img.copy(),
+                bb=bb.copy(),
+                format=Constants.BoundingBoxFormat.YOLOV8,
+                p=function_parameter["P"],
+                min_width=function_parameter["MIN_WIDTH"],
+                min_height=function_parameter["MIN_HEIGHT"],
+            )
 
         else:
-            print(f"[ERROR] function name {function_name} not found !!!")
+            print(f"\t\t Augment function {function_name} is not found.")
+            img, bb = self.prepare_output(img=img, bb=bb)
+            return img, bb
 
         img, bb = self.prepare_output(img=img, bb=bb)
 
